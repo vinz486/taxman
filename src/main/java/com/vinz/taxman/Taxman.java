d3d9d20f1d0a112d13e515e403f158d7a00d90d7
Vinz <vinz486@gmail.com>
2019-11-18 21:51:59 +0100

    Project setup

---
+++ src/main/java/com/vinz/taxman/Taxman.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+
+public class Taxman {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ src/test/java/com/vinz/taxman/TaxmanTests.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+
+@RunWith(MockitoJUnitRunner.class)
+public class TaxmanTests {
+
+	@Test
+	public void test() {
+
+	}
+
+}
